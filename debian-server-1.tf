d05747171e132df7bfc5b87abdda2d79a234dca0
efrej <erik.frej@gmail.com>
2024-09-27 16:44:51 +0200

    initial commit

---
+++ debian-server-1.tf
@@ -0,0 +1,25 @@
+resource "proxmox_vm_qemu" "debian-server-1" {
+  name = "debian-server-1"
+  desc = "Debian GUI"
+  target_node = "pve"
+
+  agent = 1
+
+  clone = "debianVM"
+  cores = 2
+  sockets = 1
+  cpu = "host"
+  memory = 2048
+
+  network {
+    bridge = "vmbr0"
+    model = "virtio"
+  }
+
+  disk {
+    storage = "local-lvm"
+    type = "disk"
+    size = "32G"
+    slot = "scsi0"
+  }
+}
